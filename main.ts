67bad34da78216547817c2a5442648c5bf73fcf5
Flak01 <102991631+Flak01@users.noreply.github.com>
2022-04-22 12:33:32 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+let Contact = 0
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+pins.analogWritePin(AnalogPin.P0, 100)
+pins.analogWritePin(AnalogPin.P1, 10)
+basic.pause(50)
+pins.analogWritePin(AnalogPin.P0, 100)
+pins.analogWritePin(AnalogPin.P1, 100)
+basic.pause(100)
+basic.forever(function () {
+    Contact = pins.digitalReadPin(DigitalPin.P2)
+    if (Contact == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        pins.analogWritePin(AnalogPin.P0, 100)
+        pins.analogWritePin(AnalogPin.P1, 10)
+        basic.pause(500)
+        pins.analogWritePin(AnalogPin.P0, 10)
+        pins.analogWritePin(AnalogPin.P1, 10)
+        basic.pause(500)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        pins.analogWritePin(AnalogPin.P0, 10)
+        pins.analogWritePin(AnalogPin.P1, 100)
+        basic.pause(50)
+    }
+})
+basic.forever(function () {
+	
+})
